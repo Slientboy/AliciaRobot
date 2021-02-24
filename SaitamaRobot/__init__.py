18f261653adcf6f1be41c543f7d5c086cb626d0c
H1M4N5HU0P <75165252+H1M4N5HU0P@users.noreply.github.com>
2021-02-25 00:05:02 +0530

    Add files via upload

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -83,6 +83,7 @@ if ENV:
     BAN_STICKER = os.environ.get('BAN_STICKER',
                                  'CAADAgADOwADPPEcAXkko5EB3YGYAg')
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
+    apiKey = os.environ.get('apiKey', False)
     CASH_API_KEY = os.environ.get('CASH_API_KEY', None)
     TIME_API_KEY = os.environ.get('TIME_API_KEY', None)
     AI_API_KEY = os.environ.get('AI_API_KEY', None)
