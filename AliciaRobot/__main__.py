bdf8aeecb67eac8a281a3ce60b919ec357d33f43
H1M4N5HU0P <75165252+H1M4N5HU0P@users.noreply.github.com>
2021-03-27 00:05:13 +0530

    Update __main__.py

--- AliciaRobot/__main__.py
+++ AliciaRobot/__main__.py
@@ -695,15 +695,7 @@ def main():
 
     # dispatcher.add_handler(test_handler)
     dispatcher.add_handler(start_handler)
-    dispatcher.add_handler(help_handler)
-    dispatcher.add_handler(about_callback_handler)
-    dispatcher.add_handler(source_callback_handler)
-    dispatcher.add_handler(admin_callback_handler)
-    dispatcher.add_handler(settings_handler)
-    dispatcher.add_handler(help_callback_handler)
-    dispatcher.add_handler(settings_callback_handler)
     dispatcher.add_handler(migrate_handler)
-    dispatcher.add_handler(donate_handler)
 
     dispatcher.add_error_handler(error_callback)
 
